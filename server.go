1ee8ed542f554a2de4df21fcafe68367bf1bbf90
lee <lee-dasilva@hotmail.com>
2020-01-08 01:18:24 +0000

    adding port from heroku

--- server.go
+++ server.go
@@ -306,6 +306,22 @@ func main() {
 	r.HandleFunc("/api/artists/{id}", getArtist).Methods("GET")
 	r.HandleFunc("/api/images/{image}", getImages).Methods("GET")
 
-	fmt.Println("Server running on port 8080")
+	port := GetPort()
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+	fmt.Printf("Server running on port%v", port)
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
